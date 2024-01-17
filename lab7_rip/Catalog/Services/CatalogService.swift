c65403cfa35ade293a8847db8b6c5da38bf9a098
Grigoriy <gri003p@mail.ru>
2024-01-17 13:45:47 +0300

    fix

--- lab7_rip/Catalog/Services/CatalogService.swift
+++ lab7_rip/Catalog/Services/CatalogService.swift
@@ -6,20 +6,17 @@
 //
 
 import Foundation
-// enum для пробрасывания ошибок
+
 enum NetworkError: Error {
     case urlError
     case emptyData
 }
 final class CatalogService {
-    private init() {} // singleton
-    static let shared = CatalogService() // singleton
+    private init() {}
+    static let shared = CatalogService()
 
-    //(completion: @escaping (Result<CatalogService, Error>) -> Void) - замыкание
-    // (Result<CatalogService, Error>) - принимает 1 из двух состояний CatalogService или Error
     func getCatalogData(with albumNumber: String?, price: String?, completion: @escaping (Result<[CatalogApiModel], Error>) -> Void) {
-//        var urlString = "http://192.168.1.70:8000/equipment/"
-        var urlString = "http://0.0.0.0:8000/equipment/"
+        var urlString = "http:/192.168.1.74/equipment/"
    
         
         if let albumNumber = albumNumber {
@@ -35,26 +32,22 @@ final class CatalogService {
             return
         }
         
-        URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in  // completionHandler – замыкание для обработки  данных  в другом слое (в данном случае  view controller)
-            //if let, guard let - разные виды развертывания опционала
+        URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
             if let error = error {
-                print("error") // внутри error != nil
+                print("error")
                 completion(.failure(error))
             }
-            // снаружи error == nil
-                           
             guard let data = data else {
-                completion(.failure(NetworkError.emptyData)) // внутри data == nil
+                completion(.failure(NetworkError.emptyData))
                 return
             }
-            // снаружи data != nil
 
             do {
-                let catalogData = try JSONDecoder().decode([CatalogApiModel].self, from: data) //декодируем json в созданную струткру с данными
+                let catalogData = try JSONDecoder().decode([CatalogApiModel].self, from: data)
                 completion(.success(catalogData))
             } catch let error {
                 completion(.failure(error))
             }
-        }).resume() // запускаем задачу
+        }).resume()
     }
 }
